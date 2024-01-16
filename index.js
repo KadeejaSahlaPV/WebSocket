4b56b0a173c322ce0f57723eda8840d5e871195b
kadeeja Sahla PV <kadeejasahlapv@gmail.com>
2024-01-16 18:52:24 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const http = require("http");
+const express = require("express");
+const path = require("path");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+// Socket.io
+io.on("connection", (socket) =>{
+    socket.on("user-message", (message) =>{
+      io.emit("message",message);
+    });  
+});
+
+app.use(express.static(path.resolve("./public")));
+
+app.get("/", (req, res) => {
+    return res.sendFile("/public/index.html");  
+});
+server.listen(7000, () => console.log(`Server started at PORT:7000`));
